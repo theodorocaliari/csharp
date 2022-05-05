d80ee41080d9ebdcdd113ae6e9ec55f3f577151d
theodorocaliari <theodorocaliari@gmail.com>
2022-05-04 21:04:11 -0300

    matrizes

---
+++ ws-vs2019/secao7-matrizes/exercicioResolvido01/exercicioResolvido01/Program.cs
@@ -0,0 +1,34 @@
+﻿int n = int.Parse(Console.ReadLine());
+int[,] matriz;
+int contador;
+
+matriz = new int[n,n];
+
+for (int i = 0; i < n; i++)
+{
+    string[] s = Console.ReadLine().Split(' ');
+    for (int j = 0; j < n; j++)
+    {
+        matriz[i, j] = int.Parse(s[j]);
+    }
+}
+
+contador = 0;
+Console.Write ("Diagonal Principal: ");
+for(int i = 0; i < n; i++)
+{
+    for(int j = 0; j < n; j++)
+    {
+        if(i == j)
+        {
+            Console.Write($"{matriz[i, j]} ");
+        }
+        if(matriz[i, j] < 0)
+        {
+            contador += 1;
+        }
+    }  
+}
+Console.WriteLine();
+
+Console.WriteLine($"Quantidade de Negativos: {contador}");
---
+++ ws-vs2019/secao7-matrizes/problemaExemplo01/problemaExemplo01/Program.cs
@@ -0,0 +1,26 @@
+﻿int linha, coluna;
+int[,] A;
+
+string[] s1 = Console.ReadLine().Split(' ');
+
+linha = int.Parse(s1[0]);
+coluna = int.Parse(s1[1]);
+A = new int[linha, coluna];
+
+for(int i = 0; i < linha; i++)
+{
+    string[] s = Console.ReadLine().Split(' '); 
+    for(int j = 0; j < coluna; j++)
+    {
+        A[i, j] = int.Parse(s[j]);
+    }
+}
+
+for(int i = 0; i < linha; i++)
+{
+    for(int j = 0; j < coluna; j++)
+    {
+        Console.Write($"{A[i,j]} ");
+    }
+    Console.WriteLine();
+}
